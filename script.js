89c2118cbc9cc6f758d21c2a2ac48a0afd1ece13
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-07-16 13:43:55 +0200

    Empezamos con los ejercicios

---
+++ script.js
@@ -0,0 +1,29 @@
+//Bienvenido/a a Javascript básico parte III, donde veremos funciones.
+
+//En el archivo index.html que tienes abierto en el navegador (preferiblemente chrome o firefox) vamos a trabajar con la consola, abre tu inspector y en la pestaña de consola, mira si puedes ver el mensaje escrito abajo.
+
+console.log("Hola, soy tu consola y juntos/as vamos a aprender JavaScript");
+
+//Si has encontrado el mensaje puedes continuar con los ejercicios
+
+//Números
+
+//Ejercicio 1: Escribe una función que tome dos números como argumentos y devuelva su suma.
+
+//Ejercicio 2:
+
+//Ejercicio 3:
+
+//Ejercicio 4:
+
+//Ejercicio 5:
+
+//Ejercicio 6:
+
+//Ejercicio 7:
+
+//Ejercicio 8:
+
+//Ejercicio 9:
+
+//Ejercicio 10:
